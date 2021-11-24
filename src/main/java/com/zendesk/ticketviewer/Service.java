55215e712b9cd86fe802d36288ded20f3a1bece8
selvaganeshmuthu <selvaganeshjmuthu@gmail.com>
2021-11-24 11:39:06 -0700

    Added default credentials.properties

--- src/main/java/com/zendesk/ticketviewer/Service.java
+++ src/main/java/com/zendesk/ticketviewer/Service.java
@@ -1,12 +1,16 @@
 package com.zendesk.ticketviewer;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.zendesk.ticketviewer.api.TicketAPI;
 import com.zendesk.ticketviewer.config.Config;
 import com.zendesk.ticketviewer.util.TicketViewUtil;
 
 public class Service {
+	
+	private static final Logger LOGGER = Logger.getLogger(Service.class.getName());
 	
 	// Maximum Number of tickets displayed per page
 	private static final int MAX_PAGE_SIZE = 25;
@@ -48,76 +52,88 @@ public class Service {
 	}
 
 	private static void getTickets(Scanner scans) {
-		int page = 0;
-		String operation = NEXT;
-		while(!operation.equals(MAIN)) {
-			// Page incremented and decremented based on operation.
-			if(operation.equals(NEXT)) {
-				page += 1;
+		try {
+			int page = 0;
+			String operation = NEXT;
+			while(!operation.equals(MAIN)) {
+				// Page incremented and decremented based on operation.
+				if(operation.equals(NEXT)) {
+					page += 1;
+				}
+				else if(operation.equals(PREVIOUS)) {
+					page -= 1;
+				}
+				else {
+					System.out.println("Invalid operation.");
+					return;
+				}
+				
+				Tickets tickets = TicketAPI.getTickets(page, MAX_PAGE_SIZE);
+				if(tickets == null) {
+					System.out.println("Cannot fetch tickets");	
+					return;
+				}
+				if(tickets.getTickets().isEmpty()) {
+					System.out.println("You have reached the end of the list");
+					return;
+				}
+				String ticketsPrint = TicketViewUtil.getTicketsPrint(tickets);
+				System.out.println(ticketsPrint);
+				System.out.println("Current page: " + page);
+				
+				if(tickets.isPrevious()) {
+					System.out.println("Enter \"" + PREVIOUS + "\" for previous ");
+				}
+				if(tickets.isNext()) {
+					System.out.println("Enter \"" + NEXT + "\" for next");
+				}
+				System.out.println("Enter \"" + MAIN + "\" for Main menu");
+				
+				operation = scans.next();
+				// User cannot enter "next" or "previous" when next or previous set of tickets are not available
+				if((!tickets.isNext() && operation.equals(NEXT)) ||
+						(!tickets.isPrevious() && operation.equals(PREVIOUS))) {
+					System.out.println("Invalid operation.");
+					return;
+				}
 			}
-			else if(operation.equals(PREVIOUS)) {
-				page -= 1;
-			}
-			else {
-				System.out.println("Invalid operation.");
-				return;
-			}
-			
-			Tickets tickets = TicketAPI.getTickets(page, MAX_PAGE_SIZE);
-			if(tickets == null) {
-				System.out.println("Cannot fetch tickets");	
-				return;
-			}
-			if(tickets.getTickets().isEmpty()) {
-				System.out.println("You have reached the end of the list");
-				return;
-			}
-			String ticketsPrint = TicketViewUtil.getTicketsPrint(tickets);
-			System.out.println(ticketsPrint);
-			System.out.println("Current page: " + page);
-			
-			if(tickets.isPrevious()) {
-				System.out.println("Enter \"" + PREVIOUS + "\" for previous ");
-			}
-			if(tickets.isNext()) {
-				System.out.println("Enter \"" + NEXT + "\" for next");
-			}
-			System.out.println("Enter \"" + MAIN + "\" for Main menu");
-			
-			operation = scans.next();
-			// User cannot enter "next" or "previous" when next or previous set of tickets are not available
-			if((!tickets.isNext() && operation.equals(NEXT)) ||
-					(!tickets.isPrevious() && operation.equals(PREVIOUS))) {
-				System.out.println("Invalid operation.");
-				return;
-			}
+		}
+		catch(Exception e) {
+			LOGGER.log(Level.SEVERE, "Cannot get tickets", e);
+			System.out.println("Cannot get tickets");
 		}
 	}
 
 	private static void getTicketDetail(Scanner scans) {
-		System.out.println("Enter \"" + MAIN + "\" for Main menu");
-		System.out.println("Enter ticket id:");
-		String operationStr = scans.next();
-		if(operationStr.equals(MAIN)) {
-			return;
+		try {
+			System.out.println("Enter \"" + MAIN + "\" for Main menu");
+			System.out.println("Enter ticket id:");
+			String operationStr = scans.next();
+			if(operationStr.equals(MAIN)) {
+				return;
+			}
+			Long operation = TicketViewUtil.parseIfLong(operationStr);
+			if(operation == null) {
+				System.out.println("Invalid operation.");
+				return;
+			}
+			// Ticket Id cannot be less than 0
+			if(operation < 0) {
+				System.out.println("Invalid ticket id");
+				return;
+			}
+			Ticket ticket = TicketAPI.getTicket(operation);
+			if(ticket == null) {
+				System.out.println("Cannot fetch ticket with given ticket id");
+				return;
+			}
+			
+			String ticketPrint = TicketViewUtil.getFullTicketDetailsPrint(ticket);
+			System.out.println(ticketPrint);
 		}
-		Long operation = TicketViewUtil.parseIfLong(operationStr);
-		if(operation == null) {
-			System.out.println("Invalid operation.");
-			return;
+		catch(Exception e) {
+			LOGGER.log(Level.SEVERE, "Cannot get ticket", e);
+			System.out.println("Cannot get ticket");
 		}
-		// Ticket Id cannot be less than 0
-		if(operation < 0) {
-			System.out.println("Invalid ticket id");
-			return;
-		}
-		Ticket ticket = TicketAPI.getTicket(operation);
-		if(ticket == null) {
-			System.out.println("Cannot fetch ticket with given ticket id");
-			return;
-		}
-		
-		String ticketPrint = TicketViewUtil.getFullTicketDetailsPrint(ticket);
-		System.out.println(ticketPrint);
 	}
 }
--- src/main/java/com/zendesk/ticketviewer/ServiceHelper.java
+++ src/main/java/com/zendesk/ticketviewer/ServiceHelper.java
@@ -6,11 +6,15 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import com.zendesk.ticketviewer.config.Config;
 
 public class ServiceHelper {
+	
+	private static final Logger LOGGER = Logger.getLogger(ServiceHelper.class.getName());
 	
 	private static final String LOGGING = "logging";
 	private static final String USERNAME = "username";
@@ -20,27 +24,31 @@ public class ServiceHelper {
 	private static final Set<String> CMD_PARAMETERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(LOGGING, USERNAME, API_TOKEN, DOMAIN)));
 
 	public static void setCommandParameters(String[] args) {
-		Map<String, String> cmdParameters = getCommandParameters(args);
-		
-		// Users can enable logging by using logging option. Disabled by default
-		if(!cmdParameters.getOrDefault(LOGGING, "false").equals("true")) { 
-			LogManager.getLogManager().reset();
+		try {
+			Map<String, String> cmdParameters = getCommandParameters(args);
+			
+			// Users can enable logging by using logging option. Disabled by default
+			if(!cmdParameters.getOrDefault(LOGGING, "false").equals("true")) { 
+				LogManager.getLogManager().reset();
+			}
+			
+			// Set the username, apitoken and domain of the account that has to be accessed. If not set data is retireved from
+			// credentials.properties file
+			if(cmdParameters.containsKey(USERNAME)) { 
+				Config.setUsername(cmdParameters.get(USERNAME));
+			}
+			
+			if(cmdParameters.containsKey(API_TOKEN)) { 
+				Config.setAPIToken(cmdParameters.get(API_TOKEN));
+			}
+			
+			if(cmdParameters.containsKey(DOMAIN)) { 
+				Config.setDomain(cmdParameters.get(DOMAIN));
+			}
 		}
-		
-		// Set the username, apitoken and domain of the account that has to be accessed. If not set data is retireved from
-		// credentials.properties file
-		if(cmdParameters.containsKey(USERNAME)) { 
-			Config.setUsername(cmdParameters.get(USERNAME));
+		catch(Exception e) {
+			LOGGER.log(Level.WARNING, "Cannot set command line parameters. Using default parameters");
 		}
-		
-		if(cmdParameters.containsKey(API_TOKEN)) { 
-			Config.setAPIToken(cmdParameters.get(API_TOKEN));
-		}
-		
-		if(cmdParameters.containsKey(DOMAIN)) { 
-			Config.setDomain(cmdParameters.get(DOMAIN));
-		}
-		
 	}
 
 	private static Map<String, String> getCommandParameters(String[] args) {
